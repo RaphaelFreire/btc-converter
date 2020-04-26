ff43c9c1d7b7cad19a606880569b5603c8be7f3d
Raphael Freire <raphaelmachadofreire@gmail.com>
2020-04-26 10:19:57 -0300

    first commit

---
+++ tests/main.spec.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect; // commonjs
+
+const exec = require('child_process').exec;
+
+const btcConverter = './src/main.js';
+
+const pkg = require('../package.json');
+
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return the description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
+      done();
+    });
+  });
+});
+
